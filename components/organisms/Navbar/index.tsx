1a413410ba261f58c08d3c2a07b6fc011a715c6b
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-02 14:38:12 +0700

    setup sign up form

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
                             <Menu title={'Rewards'} />
                             <Menu title={'Discover'} />
                             <Menu title={'Global Rank'} />
-                            <Auth isLogin />
+                            <Auth />
                         </ul>
                     </div>
                 </div>
