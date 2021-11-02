0f40670a86d101ba7246ac41fd7b2e782a52952b
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-02 11:07:23 +0700

    intregasi nominal dan banks detail voucher

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['bwa-storegg-aly.herokuapp.com'],
+        domains: ['api-bwa-storegg.herokuapp.com'],
     },
 };
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -16,20 +16,25 @@ export default function Detail() {
         },
     });
 
+    const [nominals, setNominals] = useState([]);
+    const [payments, setPayments] = useState([]);
+
     const getVoucherDetailAPI = useCallback(async (id) => {
         const data = await getDetailVoucher(id);
         console.log('data', data);
-        setDataItem(data);
+
+        setDataItem(data.detail);
+        setNominals(data.detail.nominals);
+        setPayments(data.payment);
     }, []);
 
     useEffect(() => {
         if (isReady) {
             console.log('Router sudah tersedia', query.id);
             getVoucherDetailAPI(query.id);
-        } else {
-            console.log('Router tidak tersedia');
         }
     }, [isReady]);
+
     return (
         <>
             <Navbar />
@@ -50,7 +55,10 @@ export default function Detail() {
                         <div className='col-xl-9 col-lg-8 col-md-7 ps-md-25'>
                             <TopUpItem data={dataItem} type='desktop' />
                             <hr />
-                            <TopUpForm />
+                            <TopUpForm
+                                nominals={nominals}
+                                payments={payments}
+                            />
                         </div>
                     </div>
                 </div>
