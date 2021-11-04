0097ba23c4c258a5707310a039b6218ea34a8afa
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-04 12:54:03 +0700

    refactoring config api

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -31,12 +31,12 @@ export default function SignUpPhoto() {
 
   useEffect(() => {
     const getLocalForm = localStorage.getItem('user-form');
-    setLocalForm(JSON.parse(getLocalForm));
+    setLocalForm(JSON.parse(getLocalForm!));
   }, []);
 
   const onSubmit = async () => {
     const getLocalForm = await localStorage.getItem('user-form');
-    const form = JSON.parse(getLocalForm);
+    const form = JSON.parse(getLocalForm!);
     const data = new FormData();
 
     data.append('image', image);
@@ -52,7 +52,7 @@ export default function SignUpPhoto() {
     const result = await setSignUp(data);
     console.log('Result : ', result);
 
-    if (result?.error === 1) {
+    if (result.error) {
       toast.error(result.message);
     } else {
       toast.success('Register Berhasil');
