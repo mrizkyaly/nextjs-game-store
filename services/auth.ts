1bd87bdb88120408c45ae2e84ece8fc2087833a3
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-04 08:32:07 +0700

    intregasi sign in api

--- services/auth.ts
+++ services/auth.ts
@@ -1,25 +1,46 @@
 import axios from 'axios';
+import { LoginTypes } from './data-types';
 
 const ROOT_API = process.env.NEXT_PUBLIC_API;
 const API_VERSION = 'api/v1';
 
 export async function setSignUp(data) {
-    const URL = 'auth/signup';
+  const URL = 'auth/signup';
 
-    const response = await axios
-        .post(`${ROOT_API}/${API_VERSION}/${URL}`, data)
-        .catch((err) => err.response);
+  const response = await axios
+    .post(`${ROOT_API}/${API_VERSION}/${URL}`, data)
+    .catch((err) => err.response);
 
-    console.log('api res', response);
+  const axiosResponse = response.data;
+  if (axiosResponse?.error === 1) {
+    return axiosResponse;
+  }
 
-    const axiosResponse = response.data;
-    if (axiosResponse?.error === 1) {
-        return axiosResponse;
-    }
-
-    return axiosResponse.data;
+  return axiosResponse.data;
 }
 
-export async function setLogin() {
-    return null;
+export async function setLogin(data: LoginTypes) {
+  const URL = 'auth/signin';
+
+  const response = await axios
+    .post(`${ROOT_API}/${API_VERSION}/${URL}`, data)
+    .catch((err) => err.response);
+
+  if (response?.status > 300) {
+    const res = {
+      error: true,
+      message: response.data.message,
+      data: null,
+    };
+
+    return res;
+  }
+
+  const res = {
+    error: false,
+    message: 'success',
+    data: response.data.data,
+  };
+
+  return res;
 }
--- services/data-types/index.ts
+++ services/data-types/index.ts
@@ -1,34 +1,39 @@
 export interface CategoryTypes {
-    _id: string;
-    name: string;
-    __v: number;
+  _id: string;
+  name: string;
+  __v: number;
 }
 
 export interface GameItemTypes {
-    _id: string;
-    status: string;
-    name: string;
-    thumbnail: string;
-    category: CategoryTypes;
+  _id: string;
+  status: string;
+  name: string;
+  thumbnail: string;
+  category: CategoryTypes;
 }
 
 export interface BanksTypes {
-    _id: string;
-    name: string;
-    bankName: string;
-    noRekening: string;
+  _id: string;
+  name: string;
+  bankName: string;
+  noRekening: string;
 }
 
 export interface PaymentTypes {
-    _id: string;
-    type: string;
-    status: string;
-    banks: BanksTypes[];
+  _id: string;
+  type: string;
+  status: string;
+  banks: BanksTypes[];
 }
 
 export interface NominalsTypes {
-    _id: string;
-    coinQuantity: number;
-    coinName: string;
-    price: number;
+  _id: string;
+  coinQuantity: number;
+  coinName: string;
+  price: number;
+}
+
+export interface LoginTypes {
+  email: string;
+  password: string;
 }
