c47a2d14ca717a541b0242a0f7b3c1ffcea20f25
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-05 19:33:41 +0700

    getServerSide props for checkout

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,36 +1,63 @@
+import jwtDecode from 'jwt-decode';
 import Image from 'next/image';
 import CheckoutDetail from '../components/organisms/CheckoutDetail';
 import CheckoutItem from '../components/organisms/CheckoutItem';
 import ChekoutConfirmation from '../components/organisms/ChekoutConfirmation';
+import { JWTPayloadTypes, UserTypes } from '../services/data-types';
 
-export default function Checkout() {
-    return (
-        <section className='checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30'>
-            <div className='container-fluid'>
-                <div className='logo text-md-center text-start pb-50'>
-                    <a className='' href='#'>
-                        <Image
-                            src='/icon/logo.svg'
-                            width={60}
-                            height={60}
-                            alt='logo'
-                        />
-                    </a>
-                </div>
-                <div className='title-text pt-md-50 pt-0'>
-                    <h2 className='text-4xl fw-bold color-palette-1 mb-10'>
-                        Checkout
-                    </h2>
-                    <p className='text-lg color-palette-1 mb-0'>
-                        Waktunya meningkatkan cara bermain
-                    </p>
-                </div>
-                <CheckoutItem />
-                <hr />
+interface CheckoutProps {
+  user: UserTypes;
+}
 
-                <CheckoutDetail />
-                <ChekoutConfirmation />
-            </div>
-        </section>
-    );
+export default function Checkout(props: CheckoutProps) {
+  const { user } = props;
+
+  return (
+    <section className='checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30'>
+      <div className='container-fluid'>
+        <div className='logo text-md-center text-start pb-50'>
+          <a className='' href='#'>
+            <Image src='/icon/logo.svg' width={60} height={60} alt='logo' />
+          </a>
+        </div>
+        <div className='title-text pt-md-50 pt-0'>
+          <h2 className='text-4xl fw-bold color-palette-1 mb-10'>Checkout</h2>
+          <p className='text-lg color-palette-1 mb-0'>
+            Waktunya meningkatkan cara bermain
+          </p>
+        </div>
+        <CheckoutItem />
+        <hr />
+
+        <CheckoutDetail />
+        <ChekoutConfirmation />
+      </div>
+    </section>
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const jwtToken = Buffer.from(token, 'base64').toString('ascii');
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userFromPayload: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`;
+
+  return {
+    props: {
+      user: {
+        userFromPayload,
+      },
+    },
+  };
 }
