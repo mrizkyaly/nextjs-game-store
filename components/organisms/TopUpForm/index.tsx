2870638e51082d6b4037159cd48ee08d1edf3f1b
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-05 19:51:15 +0700

    setup data checkout

--- components/organisms/TopUpForm/index.tsx
+++ components/organisms/TopUpForm/index.tsx
@@ -1,96 +1,106 @@
+import { useState } from 'react';
 import { NominalsTypes, PaymentTypes } from '../../../services/data-types';
 import NominalItem from './NominalItem';
 import PaymentItem from './PaymentItem';
 
 interface TopUpFormProps {
-    nominals: NominalsTypes[];
-    payments: PaymentTypes[];
+  nominals: NominalsTypes[];
+  payments: PaymentTypes[];
 }
 
 export default function TopUpForm(props: TopUpFormProps) {
-    const { nominals, payments } = props;
+  const [verifyID, setVerifyID] = useState('');
+  const { nominals, payments } = props;
 
-    return (
-        <form action='./checkout.html' method='POST'>
-            <div className='pt-md-50 pt-30'>
-                <div className=''>
-                    <label
-                        htmlFor='ID'
-                        className='form-label text-lg fw-medium color-palette-1 mb-10'
-                    >
-                        Verify ID
-                    </label>
-                    <input
-                        type='text'
-                        className='form-control rounded-pill text-lg'
-                        id='ID'
-                        name='ID'
-                        aria-describedby='verifyID'
-                        placeholder='Enter your ID'
-                    />
-                </div>
-            </div>
-            <div className='pt-md-50 pb-md-50 pt-30 pb-20'>
-                <p className='text-lg fw-medium color-palette-1 mb-md-10 mb-0'>
-                    Nominal Top Up
-                </p>
-                <div className='row justify-content-between'>
-                    {nominals.map((nominal) => (
-                        <NominalItem
-                            key={nominal._id}
-                            _id={nominal._id}
-                            coinQuantity={nominal.coinQuantity}
-                            coinName={nominal.coinName}
-                            price={nominal.price}
-                        />
-                    ))}
-                    <div className='col-lg-4 col-sm-6' />
-                </div>
-            </div>
-            <div className='pb-md-50 pb-20'>
-                <p className='text-lg fw-medium color-palette-1 mb-md-10 mb-0'>
-                    Payment Method
-                </p>
-                <fieldset id='paymentMethod'>
-                    <div className='row justify-content-between'>
-                        {payments.map((payment) =>
-                            payment.banks.map((bank) => (
-                                <PaymentItem
-                                    key={bank._id}
-                                    bankID={bank._id}
-                                    type={payment.type}
-                                    name={bank.bankName}
-                                />
-                            ))
-                        )}
-                    </div>
-                </fieldset>
-            </div>
-            <div className='pb-50'>
-                <label
-                    htmlFor='bankAccount'
-                    className='form-label text-lg fw-medium color-palette-1 mb-10'
-                >
-                    Bank Account Name
-                </label>
-                <input
-                    type='text'
-                    className='form-control rounded-pill text-lg'
-                    id='bankAccount'
-                    name='bankAccount'
-                    aria-describedby='bankAccount'
-                    placeholder='Enter your Bank Account Name'
+  const onNominalItemChange = (data: NominalsTypes) => {
+    console.log('Data', data);
+    localStorage.setItem('nominal-item', JSON.stringify(data));
+  };
+
+  return (
+    <form action='./checkout.html' method='POST'>
+      <div className='pt-md-50 pt-30'>
+        <div className=''>
+          <label
+            htmlFor='ID'
+            className='form-label text-lg fw-medium color-palette-1 mb-10'
+          >
+            Verify ID
+          </label>
+          <input
+            type='text'
+            className='form-control rounded-pill text-lg'
+            id='ID'
+            name='ID'
+            aria-describedby='verifyID'
+            placeholder='Enter your ID'
+            value={verifyID}
+            onChange={(event) => setVerifyID(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className='pt-md-50 pb-md-50 pt-30 pb-20'>
+        <p className='text-lg fw-medium color-palette-1 mb-md-10 mb-0'>
+          Nominal Top Up
+        </p>
+        <div className='row justify-content-between'>
+          {nominals.map((nominal) => (
+            <NominalItem
+              key={nominal._id}
+              _id={nominal._id}
+              coinQuantity={nominal.coinQuantity}
+              coinName={nominal.coinName}
+              price={nominal.price}
+              onChange={() => onNominalItemChange(nominal)}
+            />
+          ))}
+          <div className='col-lg-4 col-sm-6' />
+        </div>
+      </div>
+      <div className='pb-md-50 pb-20'>
+        <p className='text-lg fw-medium color-palette-1 mb-md-10 mb-0'>
+          Payment Method
+        </p>
+        <fieldset id='paymentMethod'>
+          <div className='row justify-content-between'>
+            {payments.map((payment) =>
+              payment.banks.map((bank) => (
+                <PaymentItem
+                  key={bank._id}
+                  bankID={bank._id}
+                  type={payment.type}
+                  name={bank.bankName}
                 />
-            </div>
-            <div className='d-sm-block d-flex flex-column w-100'>
-                <a
-                    href='/checkout'
-                    type='submit'
-                    className='btn btn-submit rounded-pill fw-medium text-white border-0 text-lg'
-                >
-                    Continue
-                </a>
-            </div>
-        </form>
-    );
+              ))
+            )}
+          </div>
+        </fieldset>
+      </div>
+      <div className='pb-50'>
+        <label
+          htmlFor='bankAccount'
+          className='form-label text-lg fw-medium color-palette-1 mb-10'
+        >
+          Bank Account Name
+        </label>
+        <input
+          type='text'
+          className='form-control rounded-pill text-lg'
+          id='bankAccount'
+          name='bankAccount'
+          aria-describedby='bankAccount'
+          placeholder='Enter your Bank Account Name'
+        />
+      </div>
+      <div className='d-sm-block d-flex flex-column w-100'>
+        <a
+          href='/checkout'
+          type='submit'
+          className='btn btn-submit rounded-pill fw-medium text-white border-0 text-lg'
+        >
+          Continue
+        </a>
+      </div>
+    </form>
+  );
 }
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -7,63 +7,61 @@ import TopUpItem from '../../components/organisms/TopUpItem';
 import { getDetailVoucher } from '../../services/player';
 
 export default function Detail() {
-    const { query, isReady } = useRouter();
-    const [dataItem, setDataItem] = useState({
-        name: '',
-        thumbnail: '',
-        category: {
-            name: '',
-        },
-    });
+  const { query, isReady } = useRouter();
+  const [dataItem, setDataItem] = useState({
+    name: '',
+    thumbnail: '',
+    category: {
+      name: '',
+    },
+  });
 
-    const [nominals, setNominals] = useState([]);
-    const [payments, setPayments] = useState([]);
+  const [nominals, setNominals] = useState([]);
+  const [payments, setPayments] = useState([]);
 
-    const getVoucherDetailAPI = useCallback(async (id) => {
-        const data = await getDetailVoucher(id);
-        console.log('data', data);
+  const getVoucherDetailAPI = useCallback(async (id) => {
+    const data = await getDetailVoucher(id);
+    console.log('data', data);
 
-        setDataItem(data.detail);
-        setNominals(data.detail.nominals);
-        setPayments(data.payment);
-    }, []);
+    setDataItem(data.detail);
+    localStorage.setItem('data-item', JSON.stringify(data.detail));
+    setNominals(data.detail.nominals);
+    setPayments(data.payment);
+  }, []);
 
-    useEffect(() => {
-        if (isReady) {
-            console.log('Router sudah tersedia', query.id);
-            getVoucherDetailAPI(query.id);
-        }
-    }, [isReady]);
+  useEffect(() => {
+    if (isReady) {
+      console.log('Router sudah tersedia', query.id);
+      getVoucherDetailAPI(query.id);
+    }
+  }, [isReady]);
 
-    return (
-        <>
-            <Navbar />
-            <section className='detail pt-lg-60 pb-50'>
-                <div className='container-xxl container-fluid'>
-                    <div className='detail-header pb-50'>
-                        <h2 className='text-4xl fw-bold color-palette-1 text-start mb-10'>
-                            Top Up
-                        </h2>
-                        <p className='text-lg color-palette-1 mb-0'>
-                            Perkuat akun dan jadilah pemenang
-                        </p>
-                    </div>
-                    <div className='row'>
-                        <div className='col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start'>
-                            <TopUpItem data={dataItem} type='mobile' />
-                        </div>
-                        <div className='col-xl-9 col-lg-8 col-md-7 ps-md-25'>
-                            <TopUpItem data={dataItem} type='desktop' />
-                            <hr />
-                            <TopUpForm
-                                nominals={nominals}
-                                payments={payments}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <section className='detail pt-lg-60 pb-50'>
+        <div className='container-xxl container-fluid'>
+          <div className='detail-header pb-50'>
+            <h2 className='text-4xl fw-bold color-palette-1 text-start mb-10'>
+              Top Up
+            </h2>
+            <p className='text-lg color-palette-1 mb-0'>
+              Perkuat akun dan jadilah pemenang
+            </p>
+          </div>
+          <div className='row'>
+            <div className='col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start'>
+              <TopUpItem data={dataItem} type='mobile' />
+            </div>
+            <div className='col-xl-9 col-lg-8 col-md-7 ps-md-25'>
+              <TopUpItem data={dataItem} type='desktop' />
+              <hr />
+              <TopUpForm nominals={nominals} payments={payments} />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
