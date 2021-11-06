16d838936d6378df3ae4f3fa8f1e0d48a8a106bb
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-06 13:22:25 +0700

    prepare data for API checkout

--- components/organisms/ChekoutConfirmation/index.tsx
+++ components/organisms/ChekoutConfirmation/index.tsx
@@ -1,20 +1,48 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 export default function ChekoutConfirmation() {
-    return (
-        <>
-            <label className='checkbox-label text-lg color-palette-1'>
-                I have transferred the money
-                <input type='checkbox' />
-                <span className='checkmark'></span>
-            </label>
-            <div className='d-md-block d-flex flex-column w-100 pt-50'>
-                <a
-                    className='btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg'
-                    href='/complete-checkout'
-                    role='button'
-                >
-                    Confirm Payment
-                </a>
-            </div>
-        </>
-    );
+  const [checkbox, setCheckBox] = useState(false);
+  const onSubmit = () => {
+    const dataItemLocal = localStorage.getItem('data-item');
+    const dataTopUpLocal = localStorage.getItem('data-topup');
+    1;
+    const dataItem = JSON.parse(dataItemLocal!);
+    const dataTopUp = JSON.parse(dataTopUpLocal!);
+
+    if (!checkbox) {
+      toast.error('Pastikan anda telah melakukan transfer pembayaran');
+    }
+    const data = {
+      voucher: dataItem._id,
+      nominal: dataTopUp.nominalItem._id,
+      payment: dataTopUp.paymentItem.payment._id,
+      bank: dataTopUp.paymentItem.bank._id,
+      name: dataTopUp.bankAccountName,
+      accountUser: dataTopUp.verifyID,
+    };
+    console.log('data', data);
+  };
+  return (
+    <>
+      <label className='checkbox-label text-lg color-palette-1'>
+        I have transferred the money
+        <input
+          type='checkbox'
+          checked={checkbox}
+          onChange={() => setCheckBox(!checkbox)}
+        />
+        <span className='checkmark'></span>
+      </label>
+      <div className='d-md-block d-flex flex-column w-100 pt-50'>
+        <button
+          className='btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg'
+          type='button'
+          onClick={onSubmit}
+        >
+          Confirm Payment
+        </button>
+      </div>
+    </>
+  );
 }
