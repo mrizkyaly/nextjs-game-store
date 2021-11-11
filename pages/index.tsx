29950bc5401f8d111c6568749afa7f556199e2cb
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-11 09:20:46 +0700

    create simple seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,38 @@ import FeaturedGame from '../components/organisms/FeaturedGame';
 import Reached from '../components/organisms/Reached';
 import Story from '../components/organisms/Story';
 import Footer from '../components/organisms/Footer';
+import Head from 'next/head';
 
 export default function index() {
-    useEffect(() => {
-        AOS.init();
-    }, []);
-    return (
-        <>
-            <Navbar />
-            <MainBanner />
-            <TransactionStep />
-            <FeaturedGame />
-            <Reached />
-            <Story />
-            <Footer />
-        </>
-    );
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>StoreGG - Get a New Experience in Gaming</title>
+        <meta
+          name='description'
+          content='Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati'
+        />
+        <meta
+          property='og:title'
+          content='StoreGG - Get a New Experience in Gaming'
+        />
+        <meta
+          property='og:description'
+          content='Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati'
+        />
+        <meta property='og:image' content='https://imageurlkalian' />
+        <meta property='og:url' content='https://storegg.com' />
+      </Head>
+      <Navbar />
+      <MainBanner />
+      <TransactionStep />
+      <FeaturedGame />
+      <Reached />
+      <Story />
+      <Footer />
+    </>
+  );
 }
